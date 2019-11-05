050d1d0a182f421880d717c8b047259a1a3a0007
Renan Oliveira <renanlido@yahoo.com.br>
2019-11-04 23:59:25 -0300

    Feature -  Queue

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -7,7 +7,8 @@ import User from '../models/User';
 import File from '../models/File';
 import Notification from '../schemas/Notification';
 
-import Mail from '../../lib/Mail';
+import CancellationMail from '../jobs/CancellationMail';
+import Queue from '../../lib/Queue';
 
 class AppointmentController {
   async index(req, res) {
@@ -155,21 +156,12 @@ class AppointmentController {
       return res.status(400).json({ error: 'Appointment not save' });
     }
 
-    const formDate = format(
-      appointment.date,
-      "'dia' dd 'de' MMMM', às' H:mm'h'",
-      { locale: ptBR }
-    );
+    /**
+     * Send Mail
+     */
 
-    await Mail.sendMail({
-      to: `${appointment.provider.name} <${appointment.provider.email}>`,
-      subject: 'Agendamento Cancelado',
-      template: 'cancellation',
-      context: {
-        provider: appointment.provider.name,
-        user: appointment.user.name,
-        date: formDate,
-      },
+    await Queue.add(CancellationMail.key, {
+      appointment,
     });
 
     return res.json(appointment);
